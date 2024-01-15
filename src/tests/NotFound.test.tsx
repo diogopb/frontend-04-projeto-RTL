e94dc6a47cce9046f1e3ccf3decf7c0284875320
Diogo Pinto <dpintobarbosa@gmail.com>
2024-01-15 12:46:35 -0300

    [Requisito 4]

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente <NotFound.tsx />', () => {
+  it('1- Testa se a página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<NotFound />);
+
+    expect(screen.getByText('Page requested not found')).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem com o texto alternativo Clefairy pushing buttons randomly with text I have no idea what i am doing.', () => {
+    renderWithRouter(<NotFound />);
+
+    expect(screen.getByAltText(/clefairy pushing buttons randomly with text i have no idea what i'm doing/i)).toHaveAttribute('src', '/404.gif');
+  });
+});
